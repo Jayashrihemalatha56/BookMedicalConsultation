1a921eae99e1e76f66ec3bd41a0e9755898b9062
Tarigopula Bhavya <tarigopula.bhavya@gds.ey.com>
2026-02-18 11:15:10 +0530

    updated

--- bookmyconsultation/src/main/java/com/ey/capstone/bookmyconsultation/entity/Address.java
+++ bookmyconsultation/src/main/java/com/ey/capstone/bookmyconsultation/entity/Address.java
@@ -2,24 +2,24 @@ package com.ey.capstone.bookmyconsultation.entity;
 
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import net.bytebuddy.dynamic.loading.InjectionClassLoader;
-import org.springframework.boot.autoconfigure.web.WebProperties;
+import org.hibernate.annotations.GenericGenerator;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
-@Data
 @Entity
+@Data
 @NoArgsConstructor
 public class Address {
-	@Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-	private String addressLine1;
-	private String addressLine2;
-	private String city;
-	private String state;
-	private String postcode;
+
+    @Id
+    @GeneratedValue(generator = "uuid2")
+    @GenericGenerator(name = "uuid2", strategy = "org.hibernate.id.UUIDGenerator")
+    @Column(name = "id", nullable = false, updatable = false, length = 36)
+    private String id;
+
+    private String addressLine1;
+    private String addressLine2;
+    private String city;
+    private String state;
+    private String postcode;
 }
